aced6d8a59d37c56a3ec025b9af0c47ebad1d0cc
Volodymyr Shuper <vshuper@gmail.com>
2026-05-24 17:15:49 +0200

    add task solution

--- src/figuresClasses.test.ts
+++ src/figuresClasses.test.ts
@@ -1,5 +1,105 @@
 import { Triangle, Rectangle, Circle, getInfo } from './figuresClasses';
 
+interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number; 
+}
+
+type Color = 'red' | 'green' | 'blue';
+
+class Triangle implements Figure {
+  shape: 'triangle' = 'triangle';
+  color: Color;
+  a: number;
+  b: number;
+  c: number;
+  
+  constructor(color: 'red' | 'green' | 'blue',
+    a: number,
+    b: number,
+    c: number) {
+    
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('your error message');
+    }
+
+    const max = Math.max(a, b, c);
+    const sum = a + b + c;
+
+    if (max >=sum - max) {
+      throw new Error('your error message');
+    }
+
+    this.color = color;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    
+    return Math.floor(area * 100) / 100;
+  }
+}
+class Rectangle implements Figure {
+  shape: 'rectangle' = 'rectangle';
+  color: Color;
+  width: number;
+  height: number;
+
+  constructor(color: 'red' | 'green' | 'blue',
+    width: number,
+    height: number) {
+    
+    if (width <= 0 || height <= 0) {
+      throw new Error('your error message');
+    }
+
+    this.color = color;
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea(): number {
+
+    const s = this.width * this.height;
+
+    return  Math.floor(s * 100) / 100;
+  }
+
+}
+class Circle implements Figure {
+  shape: 'circle' = 'circle';
+  color: Color;
+  radius: number;
+
+  constructor(color: 'red' | 'green' | 'blue',
+    radius: number) {
+    
+    if (radius <= 0) {
+      throw new Error('your error message');
+    }
+    
+    this.color = color;
+    this.radius = radius
+  }
+
+  getArea(): number {
+    const s = Math.PI * this.radius ** 2;
+
+    return Math.floor(s * 100) / 100;
+  }
+    
+}
+
+function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
+}
+
 describe('Class "Triangle"', () => {
   test('should be declared', () => {
     expect(Triangle)
